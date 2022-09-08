d93fe35ed6f2baf5c825ac65dce413a69fecae21
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-09-08 21:16:34 +0300

    added reveal/task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,7 +2,7 @@ const classLinks = Array.from(document.querySelectorAll(".menu__link"));
 
 for (let link of classLinks) {
   link.onclick = function() {
-    const parent = link.parentElement;
+    const parent = link.parentElement
     if (parent.querySelector(".menu_sub").className === "menu menu_sub"){
       parent.querySelector(".menu_sub").className = "menu menu_sub menu_active";
     }
