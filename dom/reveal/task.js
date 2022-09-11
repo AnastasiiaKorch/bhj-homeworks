38d314a399586dbea841a253d8be1f47480530c7
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-09-11 13:48:39 +0300

    fixed reveal/task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,10 +3,14 @@ const reveals = document.querySelectorAll(".reveal");
 document.addEventListener('scroll', function() {
   for (let reveal of reveals) {
     if (reveal) {
-      let top = reveal.getBoundingClientRect().top;
-      const view = window.innerHeight;
-      if (top < view) reveal.classList.add("reveal_active");
-      else reveal.classList.remove("reveal_active");
+      const { top, bottom } = reveal.getBoundingClientRect();
+      const view = window.innerHeight
+      if(bottom < 0 || top > view){
+        reveal.classList.remove("reveal_active")
+      }
+      else{
+        reveal.classList.add("reveal_active")
+      }
     }
   }
-});
+})
